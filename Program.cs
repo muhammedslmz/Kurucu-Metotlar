02942df68a917239f3b512b730a95014cec1095f
Muhammed Solmaz <muhammed.slmz@gmail.com>
2022-11-28 13:26:16 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace sinif_kavrami
+{
+    class program
+    {
+        static void Main(string []args)
+        {
+            çalışan user1 =new çalışan("Muhammed","Solmaz",25887915,"IT");
+            çalışan user2=new çalışan("İkbal","Kıyatsıl",15996315,"Muhasebe");
+            
+
+            user1.çalışanBilgileriYazdırma();
+            Console.WriteLine("*********************");
+            user2.çalışanBilgileriYazdırma();
+        }
+    }    
+    class çalışan
+    {
+        public string ad;
+        public string soyad;
+        public int no;
+        public string departman;
+
+        public çalışan(string _ad, string _soyad, int _no, string _departman)
+        {
+            this.ad=_ad;
+            this.soyad=_soyad;
+            this.no=_no;
+            this.departman=_departman;
+        }
+        public çalışan()
+        {}
+        public void çalışanBilgileriYazdırma()
+        {
+            Console.WriteLine("Çalışanın adı: "+ ad);
+            Console.WriteLine("Çalışanın soyadı: "+soyad);
+            Console.WriteLine("Çalışanın numarası: "+no);
+            Console.WriteLine("Çalışanın departmanı: "+departman);
+        }
+    }
+}
